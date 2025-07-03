c5e0db883ce6ef981e38389dd5b62b06f3e7609e
Gaurav-0801 <gaurav.pant2021@vitbhopal.ac.in>
2025-07-04 00:13:34 +0530

    init

---
+++ backend/dist/index.js
@@ -0,0 +1,86 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const express_1 = __importDefault(require("express"));
+const openai_1 = __importDefault(require("openai"));
+const cors_1 = __importDefault(require("cors"));
+const prompts_1 = require("./prompts");
+const node_1 = require("./defaults/node");
+const react_1 = require("./defaults/react");
+const openai = new openai_1.default({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c;
+    const prompt = req.body.prompt;
+    const response = yield openai.chat.completions.create({
+        model: "gpt-4o",
+        max_tokens: 200,
+        messages: [
+            {
+                role: "system",
+                content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
+            },
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
+    });
+    const answer = (_c = (_b = (_a = response.choices[0].message) === null || _a === void 0 ? void 0 : _a.content) === null || _b === void 0 ? void 0 : _b.trim()) !== null && _c !== void 0 ? _c : "";
+    if (answer === "react") {
+        res.json({
+            prompts: [
+                prompts_1.BASE_PROMPT,
+                `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            ],
+            uiPrompts: [react_1.basePrompt],
+        });
+        return;
+    }
+    if (answer === "node") {
+        res.json({
+            prompts: [
+                `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${node_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            ],
+            uiPrompts: [node_1.basePrompt],
+        });
+        return;
+    }
+    res.status(403).json({ message: "You can't access this" });
+}));
+app.post("/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
+    const messages = req.body.messages;
+    const response = yield openai.chat.completions.create({
+        model: "gpt-4o",
+        max_tokens: 8000,
+        messages: [
+            {
+                role: "system",
+                content: (0, prompts_1.getSystemPrompt)(),
+            },
+            ...messages,
+        ],
+    });
+    res.json({
+        response: (_b = (_a = response.choices[0].message) === null || _a === void 0 ? void 0 : _a.content) !== null && _b !== void 0 ? _b : "",
+    });
+}));
+app.listen(3000, () => console.log("Server running on port 3000"));
